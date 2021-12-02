7b7cdc47ffba81959c06abb2595dd35d088e82ca
Hopper <Glenn.Hopper@signetjewelers.com>
2021-12-02 08:36:47 -0500

    put all of the images into the image folder then updated the code respectively

--- hotkeys for laser.py
+++ hotkeys for laser.py
@@ -1,4 +1,4 @@
-from bot import search_and_click, found, find
+from bot import search_and_click, found, find, click_if_exists
 import keyboard
 from time import sleep
 from pyperclip import copy, paste
@@ -13,19 +13,19 @@ with open("settings.txt") as f:
         settings[key] = file_name
 
 def solve_rotational_shenanigans():
-    find("closed.png")
+    find("images\\closed.png")
     sleep(0.5)
-    if not found("selected_execute.png"):
-        search_and_click("execute.png")
-    search_and_click("show_position_start.png")
-    search_and_click("left_arrow.png")
+    if not found("images\\selected_execute.png"):
+        search_and_click("images\\execute.png")
+    search_and_click("images\\show_position_start.png")
+    search_and_click("images\\left_arrow.png")
     sleep(0.3)
-    search_and_click("right_arrow.png")
+    search_and_click("images\\right_arrow.png")
 
 def flip_stamp():
-    if not found("selected_layout.png"):
-        search_and_click("layout.png")
-    search_and_click("alpha.png", below = 20, double = True)
+    if not found("images\\selected_layout.png"):
+        search_and_click("images\\layout.png")
+    search_and_click("images\\alpha.png", below = 20, double = True)
     pyautogui.hotkey("ctrl", "a")
     pyautogui.hotkey("ctrl", "c")
     copy(str(int(paste()) + 180))
@@ -34,30 +34,30 @@ def flip_stamp():
     pyautogui.hotkey("enter")
 
 def toggle_door():
-    if found("question_door.png"):
+    if found("images\\question_door.png"):
         print("found question door")
-        search_and_click("question_door.png")
-        search_and_click("door.png")
+        search_and_click("images\\question_door.png")
+        search_and_click("images\\door.png")
         solve_rotational_shenanigans()
-    elif found("door.png"):
+    elif found("images\\door.png"):
         print("found open door")
-        search_and_click("door.png")
+        search_and_click("images\\door.png")
         solve_rotational_shenanigans()
-    elif found("closed.png"):
+    elif found("images\\closed.png"):
         print("found closed!")
-        search_and_click("closed.png")
+        search_and_click("images\\closed.png")
 
 def open_template(key):
     print("hotkey pressed:", key)
-    search_and_click("file.png", go_back = False)
-    while not found("open.png"):
+    search_and_click("images\\file.png", go_back = False)
+    while not found("images\\open.png"):
         pyautogui.click()
-    search_and_click("open.png", go_back = False)
+    search_and_click("images\\open.png", go_back = False)
     copy("C:\\Users\\ghopper\\Desktop\\stamps\\new computer\\{0}".format(settings[key]))
     pyautogui.hotkey("ctrl", "v")
     pyautogui.hotkey("enter")
-    search_and_click("surface.png")
-    search_and_click("inside_diameter.png")
+    search_and_click("images\\surface.png")
+    search_and_click("images\\inside_diameter.png")
 
 currently_down = False
 while True:
