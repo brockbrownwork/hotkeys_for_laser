8c0f754978e61cd09ad4920ec324ced86fe30308
Hopper <Glenn.Hopper@signetjewelers.com>
2021-12-03 10:49:14 -0500

    added a hotkey for changing alpha: ctrl + r

--- hotkeys for laser.py
+++ hotkeys for laser.py
@@ -22,11 +22,14 @@ def solve_rotational_shenanigans():
     sleep(0.3)
     search_and_click("images\\right_arrow.png", go_back = False)
 
-def flip_stamp():
+def change_alpha():
     if not found("images\\selected_layout.png"):
         search_and_click("images\\layout.png")
     search_and_click("images\\alpha.png", below = 20, double = True)
     pyautogui.hotkey("ctrl", "a")
+
+def flip_stamp():
+    change_alpha()
     pyautogui.hotkey("ctrl", "c")
     copy(str(int(paste()) + 180))
     sleep(0.1)
@@ -75,7 +78,9 @@ while True:
         if keyboard.is_pressed('ctrl + {0}'.format(key)):
             open_template(key)
     # start the job if the user hits "ctrl + enter"
-    if keyboard.is_pressed("ctrl + enter".format(key)):
+    if keyboard.is_pressed("ctrl + enter"):
         click_if_exists("images\\execute.png")
         click_if_exists("images\\ok.PNG")
         click_if_exists("images\\start.png")
+    if keyboard.is_pressed("ctrl + r"):
+        change_alpha()
